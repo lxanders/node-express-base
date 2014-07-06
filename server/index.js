57fb159f4eb92b6759e0ed243758f65b1432b691
Alexander Schmidt <alexanderschmidt1@gmail.com>
2014-07-06 09:56:05 +0200

    add templating support

--- server/index.js
+++ server/index.js
@@ -3,4 +3,11 @@
 var express = require('express'),
     app = express();
 
+app.set('view engine', 'hbs');
+app.set('views', process.cwd() + '/templates')
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
 module.exports = app;
