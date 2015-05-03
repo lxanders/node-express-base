814cbceffac227e24eae55761553bd598828fed2
Alexander Schmidt <alexanderschmidt1@gmail.com>
2015-05-03 10:23:22 +0200

    Move server index file to the appropriate server folder

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var app = require('./server/index');
-
-app.listen(3000);
-
-/* eslint-disable no-console */
-console.log('Server started on port 3000');
-/* eslint-enable no-console */
---
+++ server/server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var path = require('path'),
+    express = require('express'),
+    app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(process.cwd(), 'templates'));
+
+app.use('/client', express.static(path.join(__dirname, '../client')));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+module.exports = app;
