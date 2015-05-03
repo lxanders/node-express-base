e1ec6cd140fbb5b27b516d9d29f9375414dfb61b
Alexander Schmidt <alexanderschmidt1@gmail.com>
2015-05-03 21:01:29 +0200

    Extract variable for used port

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var app = require('./server');
+var app = require('./server'),
+    port = 3000;
 
-app.listen(3000);
+app.listen(port);
 
 /* eslint-disable no-console */
-console.log('Server started on port 3000');
+console.log('Server started on port ' + port);
 /* eslint-enable no-console */
