c349549d596fad44ed38152881147968a67f2568
Alexander Schmidt <alexanderschmidt1@gmail.com>
2015-11-19 22:21:40 +0100

    Remove obsolete functionality

--- server/system/createLoggerMiddleware.js
+++ server/system/createLoggerMiddleware.js
@@ -1,25 +1,9 @@
 import morgan from 'morgan';
 
-function removeLastCharacter(text) {
-    return text.slice(0, -1);
-}
-
-function removeEndingNewLineCharacter(message) {
-    if (message.length > 0) {
-        const lastCharacter = message[message.length - 1];
-
-        if (lastCharacter === '\n') {
-            return removeLastCharacter(message);
-        }
-    }
-
-    return message;
-}
-
 function getInfoStream(logger) {
     return {
         write: (message) => {
-            logger.info(removeEndingNewLineCharacter(message));
+            logger.info(message);
         }
     };
 }
