dca94e94c246e4cce8ec690eedbd368efc6c3b76
Alexander Schmidt <alexanderschmidt1@gmail.com>
2015-11-19 19:02:15 +0100

    Add additional routes

--- server/registerRoutes.js
+++ server/registerRoutes.js
@@ -1,7 +1,15 @@
 export default (router) => {
     router.get('/', (req, res) => {
-        res.render('index', { title: 'Index page' });
+        res.render('pages/home', { title: 'Index page', homePageActive: true });
+    });
+
+    router.get('/any-page', (req, res) => {
+        res.render('pages/any', { title: 'Any page', anyPageActive: true });
+    });
+
+    router.get('/dropdown-page/:selection', (req, res) => {
+        res.render('pages/dropdown', { title: 'Dropdown page', selection: req.params.selection });
     });
 
     return router;
-}
+};
